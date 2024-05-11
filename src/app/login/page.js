63ec5a49f544a342b3bb17e7d5ec86b461c88626
Cassandra Wilcox <cassie@codehangar.io>
2024-05-10 21:14:01 -0400

    login page styling

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,94 +2,41 @@
 
 import { getAuthUser, supaLogin, supaLogout } from '@/services/supa.service'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import {
-    Input,
-    Button,
-    Spacer,
-    Container,
-    Card,
-    CardBody,
-} from '@nextui-org/react'
+import { Card, CardBody, Button, Spacer } from '@nextui-org/react'
+import LoginForm from './LoginForm'
 
 export default function Login() {
     const { authUser, error, isFetching, mutationLogin, mutationLogout } =
         useData()
 
-    async function submitLogin(e) {
-        e.preventDefault()
-        const email = e.target.email.value
-        const password = e.target.password.value
+    async function submitLogin(email, password) {
         mutationLogin.mutate({ email, password })
     }
 
     return (
-        <div className="dark darkPrimary text-foreground bg-background overflow-y- min-h-screen scroll-smooth selection:bg-primary/10 selection:text-primary text-center p-4">
+        <div className="dark darkPrimary text-foreground bg-background overflow-y- min-h-screen scroll-smooth selection:bg-primary/10 selection:text-primary flex flex-col justify-center items-center p-4">
+            <h1>DISCO</h1>
+            <Spacer y={8} />
             {error && <div>Error: {error.message}</div>}
             {isFetching ? (
                 <div>Loading...</div>
             ) : !authUser ? (
-                <div>
-                    <form
-                        onSubmit={submitLogin}
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            maxWidth: 360,
-                        }}
-                    >
-                        <div className="flex items-center justify-center">
-                            <Card className="max-w-md w-full">
-                                <CardBody>
-                                    <h3 className="text-center">Login</h3>
-                                    <Spacer y={1} />
-                                    <Input
-                                        name="email"
-                                        clearable
-                                        underlined
-                                        fullWidth
-                                        color="primary"
-                                        size="lg"
-                                        placeholder="Email"
-                                        defaultValue="grailians@gmail.com"
-                                    />
-                                    <Spacer y={1.5} />
-                                    <Input
-                                        name="password"
-                                        clearable
-                                        underlined
-                                        fullWidth
-                                        color="primary"
-                                        size="lg"
-                                        placeholder="Password"
-                                        type="password"
-                                        defaultValue="password"
-                                    />
-                                    <Spacer y={1.5} />
-                                    <Button
-                                        shadow
-                                        color="primary"
-                                        auto
-                                        type="submit"
-                                    >
-                                        Sign in
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </div>
-
-                        {/* <input type="email" name="email" defaultValue="grailians@gmsail.com" />
-                <input type="password" name="password" defaultValue="password" />
-                <button type="submit">Login</button> */}
-                    </form>
-                    {/* <hr /> */}
-                    {/* <button onclick={testMeeting}>Save Test Meeting</button> */}
-                </div>
+                <Card className="max-w-md w-full">
+                    <CardBody>
+                        <LoginForm onSubmit={submitLogin} />
+                    </CardBody>
+                </Card>
             ) : (
                 <div>
                     <div>You are logged in</div>
-                    <button onClick={() => mutationLogout.mutate()}>
+                    <Button
+                        shadow
+                        color="primary"
+                        auto
+                        onClick={() => mutationLogout.mutate()}
+                    >
                         Logout
-                    </button>
+                    </Button>
                 </div>
             )}
         </div>
